91265ab7378372c52a0e714e4d0497abcc785163
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-01 22:04:52 +0900

    lesson 12 fin.

--- MyBallGame/Assets/Scripts/PaddleScript.cs
+++ MyBallGame/Assets/Scripts/PaddleScript.cs
@@ -7,12 +7,13 @@ public class PaddleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Debug.Log(transform.position.x);
     }
 }
