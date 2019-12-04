a75843bc45a4d6086f20a83d6cad2500dbcbba58
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-04 20:38:29 +0900

    lesson 15 fin.

---
+++ MyBallGame/Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _speed = Random.Range(5f, 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0f, 0f, -1 * _speed * Time.deltaTime);
+    }
+
+    private float _speed;
+}
