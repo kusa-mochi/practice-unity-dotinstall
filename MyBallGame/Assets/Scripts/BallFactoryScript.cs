ab78dba7874898ac4380e434c46eac2af7ce5527
kusa-mochi <whoatemyapplepie@gmail.com>
2019-12-04 21:14:43 +0900

    lesson 18 fin.

--- MyBallGame/Assets/Scripts/BallFactoryScript.cs
+++ MyBallGame/Assets/Scripts/BallFactoryScript.cs
@@ -9,7 +9,7 @@ public class BallFactoryScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(ball, transform.position, transform.rotation);
+        InvokeRepeating("SpawnBall", 0f, 1f);
     }
 
     // Update is called once per frame
@@ -20,6 +20,6 @@ public class BallFactoryScript : MonoBehaviour
 
     private void SpawnBall()
     {
-
+        Instantiate(ball, new Vector3(Random.Range(-5f, 5f), transform.position.y, transform.position.z), transform.rotation);
     }
 }
